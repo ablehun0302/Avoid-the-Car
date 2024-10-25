039a6c9dde36abb9b7d3da4a7ba4474935448e33
ablehun0302 <ablehun@naver.com>
2024-10-25 18:34:05 +0900

    유저 정보 입력, 업데이트 구현(템플릿)

--- Assets/Backend/BackendManager.cs
+++ Assets/Backend/BackendManager.cs
@@ -24,12 +24,26 @@ public class BackendManager : MonoBehaviour
             Debug.LogError("초기화 실패 : " + bro);
         }
 
-        Login();
+        Test();
     }
 
-    void Login()
+    void Test()
     {
         BackendLogin.Instance.GuestLogin();
+
+        BackendGameData.Instance.GameDataGet(); // 데이터 삽입 함수
+
+        // [추가] 서버에 불러온 데이터가 존재하지 않을 경우, 데이터를 새로 생성하여 삽입
+        if (BackendGameData.userData == null)
+        {
+            BackendGameData.Instance.GameDataInsert();
+        }
+
+        BackendGameData.Instance.LevelUp(); // [추가] 로컬에 저장된 데이터를 변경
+
+        BackendGameData.Instance.GameDataUpdate(); //[추가] 서버에 저장된 데이터를 덮어쓰기(변경된 부분만)
+        
+        Debug.Log("테스트 종료");
     }
 
     //게임 종료 시 로그 기록
