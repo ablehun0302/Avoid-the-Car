d445ae03fc0f0f0a1f6d149f36d870a7622d5b59
ablehun0302 <ablehun@naver.com>
2024-11-07 12:05:38 +0900

    메인화면에서 닉네임이 뜨도록 수정

--- Client/Avoid the Car/Assets/UI/NicknameButton.cs
+++ Client/Avoid the Car/Assets/UI/NicknameButton.cs
@@ -27,7 +27,7 @@ public class NicknameButton : MonoBehaviour
                 warningText.text = "*중복된 닉네임 입니다. 다른 닉네임으로 설정해주세요.";
                 break;
             default:
-                warningText.text = "*닉네임 변경에 실패하였습니다.";
+                warningText.text = "*닉네임 변경에 실패하였습니다. 형식에 맞게 설정해주세요.";
                 break;
         }
     }
